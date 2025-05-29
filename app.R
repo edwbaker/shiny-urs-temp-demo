92297eee41dd2bf0f2e712800d0cc483194136d1
Ed Baker <edwbaker@gmail.com>
2025-05-29 20:06:48 +0100

    ggplot2

--- app.R
+++ app.R
@@ -10,6 +10,8 @@
 library(shiny)
 library(NHMDE)
 library(lubridate)
+library(ggplot2)
+library(bslib)
 
 load("sensors.RData")
 
@@ -19,51 +21,64 @@ ui <- fluidPage(
     # Application title
     titlePanel("NHM Urban Research Station Soil Temperature Demo"),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-          dateRangeInput(
-            "dates",
-            "Date Range",
-            start = "2025-05-15",
-            end = "2025-05-28",
-            min = NULL,
-            max = NULL,
-            format = "yyyy-mm-dd",
-            startview = "month",
-            weekstart = 0,
-            language = "en",
-            separator = " to ",
-            width = NULL,
-            autoclose = TRUE
+    navset_tab(
+      nav_panel(
+        "Time Series",
+        sidebarLayout(
+          sidebarPanel(
+            dateRangeInput(
+              "dates",
+              "Date Range",
+              start = "2025-05-15",
+              end = "2025-05-28",
+              min = NULL,
+              max = NULL,
+              format = "yyyy-mm-dd",
+              startview = "month",
+              weekstart = 0,
+              language = "en",
+              separator = " to ",
+              width = NULL,
+              autoclose = TRUE
+            ),
+            checkboxGroupInput(
+              "sensors",
+              "Sensors",
+              choices = unique(data$sensor_id),
+              selected = c("28-00000f9d74ea")
+            ),
+            tags$label("Options"),
+            checkboxInput(
+              "DeltaT",
+              "Show Delta T",
+              value = FALSE
+            ),
+            selectInput(
+              "DailyExtremes",
+              "Show daily extremes",
+              choices = c("None",unique(data$sensor_id))
+            )
           ),
-          selectInput(
-            "sensors",
-            "Sensors",
-            choices = unique(data$sensor_id),
-            selected = c("28-00000f9d74ea")
-          ),
-          checkboxInput(
-            "DeltaT",
-            "Show Delta T",
-            value = FALSE
-          ),
-          checkboxInput(
-            "DailyExtremes",
-            "Show daily extremes",
-            value = FALSE
-          )
-        ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("timeSeries"),
-           htmlOutput("timeSeriesNotes"),
-           htmlOutput("AccD"),
-           htmlOutput("deltaThead"),
-           plotOutput("deltaT")
+          # Show a plot of the generated distribution
+          mainPanel(
+            plotOutput("timeSeries"),
+            htmlOutput("timeSeriesNotes"),
+            htmlOutput("AccD"),
+            htmlOutput("deltaThead"),
+            plotOutput("deltaT")
+          )
         )
-    )
+      ),
+      nav_panel(
+        "Summary Stats",
+        mainPanel(
+          htmlOutput("summaryStats")
+        )
+      )
+    ),
+    id = "tab"
+
 )
 
 # Define server logic required to draw a histogram
@@ -74,26 +89,28 @@ server <- function(input, output) {
       end <- as.POSIXct(input$dates[2])+86400
       plot_data <- data[data$timestamp >= start & data$timestamp <= end, ]
       plot_data <- plot_data[plot_data$sensor_id %in% input$sensors, ]
-      plot(
-        plot_data$timestamp,
-        plot_data$value,
-        col=factor(plot_data$sensor_id),
-        type="p",
-        cex=0.3,
-        xlab="Date",
-        ylab="Temperature"
-        )
-      if (input$DailyExtremes) {
-        dw <- daily_delete_missing(plot_data, "timestamp", 60*60)
+
+      plot <- ggplot(data=plot_data, aes(x=timestamp, y=value, colour=sensor_id)) + geom_point(size=0.05) + guides(colour = guide_legend(override.aes = list(size=5)))
+
+      if (input$DailyExtremes != "None") {
+        start <- as.POSIXct(input$dates[1])
+        end <- as.POSIXct(input$dates[2])+86400
+        plot_data <- data[data$timestamp >= start & data$timestamp <= end, ]
+        dw <- plot_data[plot_data$sensor_id == input$DailyExtremes, ]
+        dw <- daily_delete_missing(dw, "timestamp", 60*60)
 
         de <- daily_extremes(dw$value, dw$timestamp, breaks=T)
 
-        abline(v=de$date[de$type=="max"], col="red", lwd=1)
-        abline(v=de$date[de$type=="min"], col="blue", lwd=1)
+        plot <- plot +
+          geom_vline(xintercept=de$date[de$type=="max"], colour="red")  +
+          geom_vline(xintercept=de$date[de$type=="min"], colour="blue")
+
       }
+
+      print(plot)
     })
     output$timeSeriesNotes <- renderUI({
-      if (input$DailyExtremes) {
+      if (input$DailyExtremes != "None") {
         p("Daily extremes are only shown for days with complete data.")
       }
     })
@@ -108,10 +125,31 @@ server <- function(input, output) {
       hour(end) <- 23
       plot_data <- data[data$timestamp >= start & data$timestamp <= end, ]
       plot_data <- plot_data[plot_data$sensor_id %in% input$sensors, ]
-      tags$div(
-        tags$h3("Accumulated Degree-Days"),
-        tags$p(paste0("Total Accumulated Degree Days: ", round(AccDD(plot_data$timestamp, plot_data$value), 2)))
-      )
+
+      title <- tags$h3("Accumulated Degree-Days")
+
+      tags_list <- lapply(unique(plot_data$sensor_id), function(item) {
+        tags$p(
+          paste(#
+            item,
+            ": ",
+            round(
+              AccDD(
+                plot_data$timestamp[plot_data$sensor_id == item],
+                plot_data$value[plot_data$sensor_id == item]
+              ),
+              2
+            ),
+            "°C days"
+          )
+        )
+      })
+      tags_list <- c(list(title), tags_list)
+
+      do.call(tagList, tags_list)
+
+
+
     })
     output$deltaThead <- renderUI({
       if (input$DeltaT) {
@@ -129,6 +167,21 @@ server <- function(input, output) {
         plot_data <- plot_data[plot_data$sensor_id %in% input$sensors, ]
         boxplot(diff(as.numeric(plot_data$timestamp)), horizontal=T)
       }
+    })
+    output$summaryStats <- renderUI({
+      tags$div(
+        tags$h3("Summary Counts"),
+        tags$table(
+          tags$tr(
+            tags$td("Total Sensors:"),
+            tags$td(length(unique(data$sensor_id)))
+          ),
+          tags$tr(
+            tags$td("Total Readings:"),
+            tags$td(nrow(data))
+          )
+        )
+      )
     })
 }
 
